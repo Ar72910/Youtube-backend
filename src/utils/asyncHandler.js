e9c40ebd767bfc90c611601a6726d1178238bbca
Ar72910 <araj30577@gmail.com>
2024-04-26 18:16:27 +0530

    add utils folder

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).
+        catch((err)=>next(err))        
+    }
+    
+}
+
+export { asyncHandler }
+
+
+
+
+// const handler = (fun) => {
+//     async (req, res, next) => {
+//         try {
+//             await fun(req,res,next)
+            
+//         }
+//         catch (err) {
+//             res.status(err.code || 500).json({
+//                 success: false,
+//                 message:err.message
+//             })
+            
+//         }
+//     }
+// }
